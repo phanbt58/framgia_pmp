3f2651b255430a44e5012fdb49052dca82d866e2
nguyenthaison <nguyenthaison9193@gmail.com>
2016-05-11 15:46:39 +0700

    task show project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,12 @@
 class ProjectsController < ApplicationController
+  load_and_authorize_resource
+
   def index
-    if current_user.manager?
-      @projects = Project.all
-    else
-      @projects = Project.list_by_assignee current_user
-    end
+    @projects = Project.list_by_assignee current_user
+  end
+
+  def show
+    @manager = @project.manager
+    @assignees = Assignee.list_by_project @project
   end
 end
--- app/models/assignee.rb
+++ app/models/assignee.rb
@@ -1,4 +1,8 @@
 class Assignee < ActiveRecord::Base
   belongs_to :sprint, class_name: Sprint.name
   belongs_to :user
+
+  scope :list_by_project, ->project{where project_id: project.id}
+
+  delegate :name, to: :user, prefix: true, allow_nil: true
 end
