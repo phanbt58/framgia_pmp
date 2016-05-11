9483ff62b391eaef091d4b49aac6cc2b625cf85f
NgocVu1994 <ngocvu.uet@gmail.com>
2016-05-11 17:28:25 +0700

    Edit project

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -16,6 +16,16 @@ class Admin::ProjectsController < ApplicationController
     @assignees = Assignee.list_by_project @project
   end
 
+  def update
+    if @project.update_attributes project_params
+      flash[:success] = flash_message "updated"
+      redirect_to admin_root_url
+    else
+      flash[:failed] = flash_message "not_updated"
+      render :edit
+    end
+  end
+  
   private
   def project_params
     params.require(:project).permit Project::PROJECT_ATTRIBUTES_PARAMS
