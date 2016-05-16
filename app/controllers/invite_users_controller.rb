0f5c86c541be82dd30de8b6ad47d25917e3d6b7a
truongkma <nguyendactruong.kma@gmail.com>
2016-05-16 11:21:28 +0700

    invite user

---
+++ app/controllers/invite_users_controller.rb
@@ -0,0 +1,36 @@
+class InviteUsersController < ApplicationController
+  before_action :load_user, only: [:edit, :update]
+
+  def create
+    @user = User.find_by email: params[:invite_user][:email].downcase
+    if @user
+      @user.create_invite_digest
+      @user.send_password_reset_email
+      flash[:success] = t "flashs.password_reset"
+      redirect_to root_url
+    else
+      flash[:failed] = t "flashs.not_email"
+      redirect_to new_invite_user_url
+    end
+  end
+
+  def update
+    if @user.update_attributes password_params
+      sign_in @user
+      flash[:success] = t "flashs.reset_success"
+      redirect_to @user
+    else
+      flash[:failed] = flash_message "not_updated"
+      render :edit
+    end
+  end
+
+  private
+  def load_user
+    @user = User.find_by email: params[:email]
+  end
+
+  def password_params
+    params.require(:user).permit User::PASSWORD_ATTRIBUTES_PARAMS
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :reset_token
   has_many :assignees
   has_many :time_logs
   has_many :activities
@@ -8,12 +9,29 @@ class User < ActiveRecord::Base
     :rememberable, :trackable, :validatable
 
   USER_ATTRIBUTES_PARAMS = [:name, :email, :password, :password_confirmation]
+  PASSWORD_ATTRIBUTES_PARAMS = [:password, :password_confirmation]
 
   enum role: [:member, :leader, :manager]
 
   validates :name, presence: true, length: {maximum: 50}
 
   before_create :set_default_role
+
+  def User.digest string
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def create_invite_digest
+    self.reset_token = SecureRandom.urlsafe_base64
+    update_attributes(reset_password_token: User.digest(reset_token),
+      reset_password_sent_at: Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.invite_user(self).deliver_now
+  end
 
   private
   def set_default_role
