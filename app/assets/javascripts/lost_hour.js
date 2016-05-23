5dfd9ad31030f3eedc82a26ddc3174dbe17fe18f
HoangNam <hnam2812@gmail.com>
2016-05-23 17:44:22 +0700

    Task sum total lost hour

---
+++ app/assets/javascripts/lost_hour.js
@@ -0,0 +1,33 @@
+function calculate_lost_hour(col) {
+  var total = 0;
+  var number_assignees = $("#lost_hour_table").data("numberAssignees");
+
+  for(var j = 0; j < number_assignees; j++) {
+    var value = $($(".assignee-col-" + col)[j]).children().last().val();
+    if(value == "") value = 0;
+    total += parseInt(value);
+  }
+  
+  if(col == 0) {
+    $("#work-hour-" + col + " input").val(total);
+  }
+  else {
+    $("#lost-hour-" + col + " input").val(total);
+    total_lost_hour();
+  }
+}
+
+function total_lost_hour() {
+  var total = 0;
+  var work_day = $("#lost_hour_table").data("numberWorkDay");
+
+  for(var i = 1; i < work_day; i++) {
+    total += parseInt($("#lost-hour-" + i +" input").val());
+  }
+  $("#lost-hour-0 input").val(total);
+}
+
+$(document).on("change click", "#lost_hour_table td input", function(){
+  var col = parseInt($($(this).parent().attr("class").split("-")).last()[0]);
+    calculate_lost_hour(col);
+});
--- app/helpers/sprint_helper.rb
+++ app/helpers/sprint_helper.rb
@@ -2,4 +2,17 @@ module SprintHelper
   def get_work_date work_date, sprint
     work_date % sprint.work_day
   end
+
+  def sum_lost_hour
+    sum_lost_hour = []
+    @sprint.work_day.times do |day|
+      lost_hour = TimeLog.total_lost_hour @sprint, day
+      sum_lost_hour << lost_hour
+    end
+    sum_lost_hour
+  end
+
+  def total_lost_hour
+    sum_lost_hour.drop(1).inject {|total, lost_hour| total + lost_hour}
+  end
 end
