cf210e17d4440dab510284680d107ad0553df2f0
hiennv94 <hiennv.th@gmail.com>
2016-05-23 15:36:58 +0700

    caculate formula estimate times

--- app/assets/javascripts/activities.js
+++ app/assets/javascripts/activities.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();
+});
 
+$(document).on("page:change", function(){
   $("#load_more").click(function() {
     $("#activities tbody").append($("#more-rows table tbody").children().clone());
   });
