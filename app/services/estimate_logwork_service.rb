f2b14a2d1f33e1d65198f1f405a631a35fced203
truongkma <nguyendactruong.kma@gmail.com>
2016-06-02 12:25:43 +0700

    refector-server-caculate

--- app/services/estimate_logwork_service.rb
+++ app/services/estimate_logwork_service.rb
@@ -5,6 +5,8 @@ class EstimateLogworkService
   def initialize activities, sprint
     @activities = activities
     @sprint = sprint
+    @sum_estimate = sum_logwork_day 0
+    @arr_remaining_time = get_time_remaining_team
   end
 
   def get_sum_remaining
@@ -14,31 +16,31 @@ class EstimateLogworkService
   end
 
   def get_sum_worked
-    get_estimate_activities - get_sum_remaining
+    @sum_estimate - get_sum_remaining
   end
 
   def get_percent_worked
-    unless get_estimate_activities == 0
-      return number_to_percentage(get_sum_worked.to_f / get_estimate_activities * 100,
+    unless @sum_estimate == 0
+      return number_to_percentage(get_sum_worked.to_f / @sum_estimate * 100,
         precision: 0)
     end
     return 0
   end
 
   def get_percent_remaining
-    unless get_estimate_activities == 0
-      return number_to_percentage(get_sum_remaining.to_f / get_estimate_activities * 100,
+    unless @sum_estimate == 0
+      return number_to_percentage(get_sum_remaining.to_f / @sum_estimate * 100,
         precision: 0)
     end
     return 0
   end
 
   def get_estimate_activities
-    @activities.inject(0) do |sum, activity|
-      log_works = activity.log_works.order(day: :asc)
-      sum + (log_works.first.remaining_time.present? ?
-        log_works.first.remaining_time : 0)
-    end
+    @sum_estimate
+  end
+
+  def get_time_remaining
+    @arr_remaining_time
   end
 
   def sum_logwork_day day
