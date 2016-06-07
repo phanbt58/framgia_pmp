79f6883c318952fc3d135b75e0e711020bba4405
ngocvt <ngocvu.uet@gmail.com>
2016-06-07 14:34:03 +0700

    change date in master sprint

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -49,7 +49,23 @@ $(document).on("page:change", function(){
     format: I18n.t("date.day"),
     autoclose: true
   }).on("changeDate", function(event){
-    $(this).prev().val(event.format(I18n.t("date.format")));
+    var old_date = $(this).prev().val();
+    var new_date = $(this).prev().val(event.format(I18n.t("date.js_format")));
+    var changeDate = new Date(new_date.val()).getTime() - new Date(old_date).getTime();
+    var filterRow = function(v) {return v.indexOf('day-') == 0};
+    var colClass = this.className.split(' ').filter(filterRow)[0];
+    var cells = $('input[class*="master-sprint-day"]');
+    var i = parseInt(colClass.split('-')[1]) + 1;
+    for(i; i <= cells.length; i++ ) {
+      var next_date = $(".day-" + i).prev().val();
+      var tmp = new Date(next_date);
+      var next_date_new = new Date(tmp.getTime() + changeDate);
+      var d = next_date_new.getDate();
+      var m = next_date_new.getMonth() + 1;
+      var y = next_date_new.getFullYear();
+      $('.day-' + i).prev().val(y + '-' + m + '-' + d);
+      $('.day-' + i).datepicker('update', new Date(y, m, d));
+    }
   });
 
   $( "#assignee" ).select2({
