c6e5c9c1f92447896bbb2bdde2df06d2259daeb5
ngocvt <ngocvu.uet@gmail.com>
2016-06-20 09:52:20 +0700

    fix bug when remove assignee

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -1,11 +1,11 @@
 class Sprint < ActiveRecord::Base
   belongs_to :project
 
-  has_many :assignees
-  has_many :users, through: :assignees
+  has_many :assignees, dependent: :destroy
+  has_many :users, through: :assignees, before_remove: :destroy_time_logs
+  has_many :time_logs
   has_many :product_backlogs
   has_many :activities
-  has_many :time_logs
   has_many :log_works
   has_many :master_sprints
 
@@ -63,5 +63,9 @@ class Sprint < ActiveRecord::Base
 
   def include_assignee? current_user
     self.assignees.map{|assignee| assignee.user_id}.include? current_user.id
+  end
+
+  def destroy_time_logs user
+    user.assignees.find_by(sprint_id: self.id).time_logs.destroy_all
   end
 end
