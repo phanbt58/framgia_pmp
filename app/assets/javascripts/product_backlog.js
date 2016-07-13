c23f886027077c0ebec317533b7143a416d87439
DatDaiGia <vandat2912@gmail.com>
2016-07-13 15:10:00 +0700

    Add more row product backlog

--- app/assets/javascripts/product_backlog.js
+++ app/assets/javascripts/product_backlog.js
@@ -14,3 +14,23 @@ $(document).on("click", ".delete-product-backlog", function(e){
       }
     });
 });
+
+$(document).ready(function(){
+  $("#product_backlog").on("click", "#add-more-row", function(e){
+    var project_id = $(this).find("span").attr("project_id");
+    var url = $(this).attr("href");
+    $.ajax({
+      type: "POST",
+      url:  url,
+      dataType: "json",
+      data: {id: project_id},
+      success: function(result) {
+        $("table#product_backlogs tbody").append(result.content);
+      },
+      error: function(){
+        $("#notify-message").text(I18n.t("product_backlogs.delete.failed")).css("color", "red");
+      }
+    });
+    return false;
+  });
+});
