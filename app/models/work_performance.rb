d8b07b75e0f294e10486c6cd627f92e7d252dd41
TaoLuyen <taoluyen.mta@gmail.com>
2016-07-18 11:48:11 +0700

    Input form Work Performance Data

--- app/models/work_performance.rb
+++ app/models/work_performance.rb
@@ -2,5 +2,6 @@ class WorkPerformance < ActiveRecord::Base
   belongs_to :phase
   belongs_to :activity
 
-  ATTRIBUTES_PARAMS = [:description, :plan, :actual]
+  ATTRIBUTES_PARAMS = [:description, :plan, :actual, :spent_hour,
+    :burned_hour, :estimated_story, :burned_story, :estimated_task]
 end
