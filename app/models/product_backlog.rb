174b97e2f38351b2f3b6a64e9b72f6454de893b5
nhunght <nhunghoang1609@gmail.com>
2016-11-02 09:54:30 +0700

    fix-layout-project

--- app/models/product_backlog.rb
+++ app/models/product_backlog.rb
@@ -16,17 +16,21 @@ class ProductBacklog < ActiveRecord::Base
   end
 
   def total_remaining_time
-    remaining = Task.includes(:log_works)
-      .of_product_backlog_and_sprint(id, sprint_id).map do |task|
-      task.log_works.last.remaining_time
-    end.sum rescue 0
-    self.update_attributes remaining: remaining
-    remaining
+    if self.tasks.any?
+      remaining = Task.includes(:log_works)
+        .of_product_backlog_and_sprint(id, sprint_id).map do |task|
+        task.log_works.last.remaining_time
+      end.sum rescue 0
+      self.update_attributes remaining: remaining
+      remaining
+    end
   end
 
   def calculate_actual_time
-    actual_time = total_estimation_time
-    self.update_attributes actual: actual_time
-    actual_time
+    if self.tasks.any?
+      actual_time = total_estimation_time
+      self.update_attributes actual: actual_time
+      actual_time
+    end
   end
 end
